1e56baebc57163680a9f5be9cd76802ff7f259c4
bencsicszoli <171580038+bencsicszoli@users.noreply.github.com>
2025-06-11 14:33:13 +0200

    Feat(backend) ControllerAdvice

--- backend/src/test/java/com/codecool/huszonegy/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/codecool/huszonegy/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.codecool.huszonegy.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BackendApplicationTests {
 
 	@Test
