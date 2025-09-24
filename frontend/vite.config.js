51ec4d803fe3668aed68d4df65045f2f71881e4b
dkisb <177304332+dkisb@users.noreply.github.com>
2025-09-24 15:37:08 +0200

    deploy: created render deployement for backend and connected it to the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        // eslint-disable-next-line no-undef
+        target: process.env.VITE_API_URL || 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
